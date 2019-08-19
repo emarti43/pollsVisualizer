54b45e6a96a2bd1ecb540bed5ebed2afef29a0a9
bastion6429 <emarti43@ucsc.edu>
2019-08-19 14:59:41 -0700

    added bars for poll numbers

--- public/js/animateStuff.js
+++ public/js/animateStuff.js
@@ -1,7 +1,14 @@
+
 anime({
-  targets: 'div',
-  translateX: 250,
-  rotate: '1turn',
-  backgroundColor: '#FFF',
-  duration: 800
+  targets: document.querySelectorAll("li.subpopulation-response"),
+  translateX: 0,
+  duration: 600,
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let progressBars = document.querySelectorAll('.progress-bar');
+  progressBars.forEach(function(element) {
+    let barValue = (element.getAttribute('aria-valuenow') / element.getAttribute('aria-valuemax'))* 100;
+    element.style.width = `${barValue}%`;
+  });
 });
