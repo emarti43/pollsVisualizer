957534d37f8d52efea90dc7f6506f69f4796fdc8
bastion6429 <emarti43@ucsc.edu>
2019-08-19 18:16:01 -0700

    added parameter for handling pages

--- server.js
+++ server.js
@@ -4,20 +4,27 @@ const port = 8080;
 const axios = require('axios');
 const baseURL = 'https://elections.huffingtonpost.com/pollster/api/v2/'
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.set('view engine', 'pug')
-
-app.get('/', (req, res) => {
-  var polls = []
-  axios.get(baseURL + 'polls')
+app.set('view engine', 'pug');
+app.get('/:cursor?', (req, res) => {
+  let data = {};
+  if(!!req.params.cursor) {
+    data = { cursor: req.params.cursor };
+    console.log('yeah');
+  }
+  axios.get(baseURL + 'polls', data)
   .then(response => {
     res.render('index', {
       title: 'Polls',
-      items: response.data.items
+      items: response.data.items,
+      nextPage: response.data.next_cursor
     });
-  }).catch(error => {
+  })
+  .catch(error => {
     console.log(error);
+    res.write('error');
+    res.end();
   });
 });
 
